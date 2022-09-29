6fddc49dfd6d512c64c67d2961345eac2cc3f12f
jj-quesada <jj.quesadaacosta@hotmail.com>
2022-09-29 09:55:22 +0100

    V4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,8 +7,8 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        // Integer[] data = {1, 6, 2, 8, 3, 8, 2, 7, 3, 4, 2, 9, 0, 6};
-        String[] data = {"Itamar", "Álvaro", "Gema", "Álvaro", "Sol", "Álvaro", "Itamar", "Pepe"};
+        Integer[] data = {1, 6, 2, 8, 3, 8, 2, 7, 3, 4, 2, 9, 0, 6};
+        // String[] data = {"Itamar", "Álvaro", "Gema", "Álvaro", "Sol", "Álvaro", "Itamar", "Pepe"};
         
         HistogramGenerator histogramGenerator = new HistogramGenerator(data);
         
